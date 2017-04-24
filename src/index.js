c3d419ff0338e1ad030980607b42b10847652748
Jon Kuperman <jon.kuperman@gmail.com>
2017-04-24 00:39:28 -0700

    run prettier

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 const htmlparser = require('htmlparser2');
 
-function parse (HTMLString, isPartial) {
-  let results
-  let hasTitle = false
+function parse(HTMLString, isPartial) {
+  let results;
+  let hasTitle = false;
 
   const handler = new htmlparser.DomHandler((error, dom) => {
-    if (error)
-      throw new Error('Invalid HTML passed to a11y-string')
-    else
-      results = dom
-  })
+    if (error) throw new Error('Invalid HTML passed to a11y-string');
+    else results = dom;
+  });
 
-  const parser = new htmlparser.Parser(handler)
+  const parser = new htmlparser.Parser(handler);
   parser.write(HTMLString);
   parser.end();
 
   // Run all tests here for now
-  results.forEach((node) => {
+  results.forEach(node => {
     if (node.name === 'img' && node.attribs.alt === undefined)
-      throw new Error('All images must have an alt attribute')
-    if (node.name === 'title')
-      hasTitle = true
+      throw new Error('All images must have an alt attribute');
+    if (node.name === 'title') hasTitle = true;
   });
 
   // DOM should contain a title tag only if not a partial
   if (!hasTitle && !isPartial)
-    throw new Error('The web page should have a title that describes topic or purpose')
-  return results
+    throw new Error(
+      'The web page should have a title that describes topic or purpose'
+    );
+  return results;
 }
 
 module.exports = parse;
--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,29 @@
 const a11y = require(__dirname + '/../src/index');
 
 test('returns html as open tag', () => {
-  expect(a11y('<html></html>', true)).toEqual(
-    [{
+  expect(a11y('<html></html>', true)).toEqual([
+    {
       type: 'tag',
       name: 'html',
       attribs: {},
       children: [],
       next: null,
       prev: null,
-      parent: null }
-    ]);
+      parent: null,
+    },
+  ]);
 });
 
 test('throws error if an image is missing an alt attribute', () => {
   expect(() => {
-    a11y('<img src="foo.jpg" />', true)
+    a11y('<img src="foo.jpg" />', true);
   }).toThrow('All images must have an alt attribute');
-})
+});
 
 test('throws error if a full page is missing a title', () => {
   expect(() => {
-    a11y('<html><head></head><body><div>No title here</div></body></html>')
-  }).toThrow('The web page should have a title that describes topic or purpose');
-})
+    a11y('<html><head></head><body><div>No title here</div></body></html>');
+  }).toThrow(
+    'The web page should have a title that describes topic or purpose'
+  );
+});
